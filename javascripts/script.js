40afbfd0d525c8e54c83c2742a58a15b22b84466
Adam D <kirbylover4000@gmail.com>
2015-07-13 04:29:42 +0930

    remove generated files

--- javascripts/script.js
+++
@@ -1,8 +0,0 @@
-(function(document, window, undefined) {
-	document.documentElement.classList.add("js");
-
-	$("#js-navbutton").on("click", function(e) {
-		document.body.classList.toggle("nav-showing");
-		e.preventDefault();
-	});
-})(document, window);
